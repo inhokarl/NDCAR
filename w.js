6c70344e33570bfc101b282fb3b590464414726c
inhokarl <inhokarl90@gmail.com>
2025-07-17 21:54:16 -0300

    Update w.js

--- w.js
+++ w.js
@@ -1,24 +1,21 @@
 const cacheName = "ndcar-v1";
 const filesToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/logo-topo.png",
+  "/NDCAR/",
+  "/NDCAR/index.html",
+  "/NDCAR/manifest.json",
+  "/NDCAR/w.js",
+  "/NDCAR/logo-topo.png",
   "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
 ];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
-    })
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
